b0452f1eef59f6ac12c8489a15e05e1bd3077184
gmmorgan1996 <griffin.mrogan@gmail.com>
2022-12-09 11:46:36 -0500

    Added cart page and not found functionality

--- src/app/components/partials/header/header.component.ts
+++ src/app/components/partials/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-header',
@@ -6,8 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  cartQuantity:number = 0;
+  constructor(cartService:CartService) {
+    cartService.getCartObservable().subscribe((cart) => {
+      this.cartQuantity = cart.totalCount;
+    });
+  }
 
   ngOnInit(): void {
   }
